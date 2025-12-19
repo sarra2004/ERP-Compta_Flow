a4fc2ab764cfc25cec79b248fdfcfa7dd6d96695
sarra2004 <sarraharbi2023@gmail.com>
2025-12-19 11:39:27 +0100

    Initial Spring Boot project setup

---
+++ compta_flow/src/main/java/com/example/erp/compta_flow/ComptaFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.erp.compta_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComptaFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComptaFlowApplication.class, args);
+	}
+
+}
---
+++ compta_flow/src/test/java/com/example/erp/compta_flow/ComptaFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.erp.compta_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComptaFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
